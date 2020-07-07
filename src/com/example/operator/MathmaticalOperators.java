96eee30cb544507415f26194f8327f9cf9ddf535
RFNshare <rfnshare@gmail.com>
2020-07-07 12:42:12 +0600

    First Commit

---
+++ src/com/example/operator/MathmaticalOperators.java
@@ -0,0 +1,7 @@
+package com.example.operator;
+
+public class MathmaticalOperators {
+    public static void main(String[] args) {
+        System.out.println("Hello, World from IntelliJ Idea!");
+    }
+}
